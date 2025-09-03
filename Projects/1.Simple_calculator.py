a8aa26d3e1c3cdf8a03005df0f93f428c1b59bd9
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-03 16:49:33 +0200

    simple calculator

---
+++ Projects/1.Simple_calculator.py
@@ -0,0 +1,12 @@
+a=20
+b=5
+
+print("Addition of ",a ,"+" ,b, "\n=",a+b)
+print("Subtraction of",a ,"-" ,b ,"\n=",a-b)
+print("Multiplication of ",a ,"*" ,b ,"\n=",a*b)
+print("Division of ",a ,"/", b ,"\n=",a/b)
+print("Modulus of ",a ,"%", b, "\n=",a%b)
+print("Exponeential of ",a ,"**", b ,a**b)
+
+
+
