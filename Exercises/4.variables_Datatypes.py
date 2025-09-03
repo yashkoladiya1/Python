bafdcaefe0f16fae9c5b921fa081e871ac01a466
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-03 15:32:54 +0200

    variable and data types

---
+++ Exercises/4.variables_Datatypes.py
@@ -0,0 +1,14 @@
+a = 2
+b = "Yash"
+c = True
+d = None
+list1 =[5,34,53,[-2,45],["apple","banana"]]
+tuple1 =(("parrot","lion"),("tiger","sparrow"))
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(list1))
+print(type(tuple1))
