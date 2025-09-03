8f1db96ec4409616029e14a5cdc33ca39e8f6296
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-03 17:18:44 +0200

    Typecasting

---
+++ Exercises/6.Typecasting.py
@@ -0,0 +1,13 @@
+string ="14"
+number = 6
+string_number = int(string)
+
+sum = number + string_number
+
+print("The sum of both number is :",sum)
+
+#Implicit Typecasting
+c= 1.34
+d= 2
+
+print("Implicit Typecasting ==",c + d)
