daf71c130c1df632faae260668fb50399007ecf9
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-04 17:20:11 +0200

    If ifelse nested ifelse

---
+++ Exercises/12.ifElse.py
@@ -0,0 +1,11 @@
+#If condition
+a = int(input("Enter Your Age:"))
+
+#Conditional operators
+# >, <, >=, <=, ==, !=
+
+if(a>=18):
+    print("you can drive")
+else:
+    print("you cannot drive")
+
---
+++ Exercises/13.IfElif.py
@@ -0,0 +1,10 @@
+#if elif
+
+num = int(input("Enter the Number:"))
+
+if(num>=0):
+    print("Number is Positive")
+elif(num==0):
+    print("Number is zero")
+else:
+    print("Number is negative")
