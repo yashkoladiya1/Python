df670efd5de2cd4a382db95d0a0177ea35787b64
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-04 18:16:56 +0200

    Forloops

---
+++ Exercises/17.ForLoops.py
@@ -0,0 +1,16 @@
+colors = ["Red","Green","Yellow","Brown"]
+
+for color in colors:
+    print(color)
+    for i in color:
+      print(i)
+
+# Range
+
+for k in range(1,10):
+   print(k)
+
+# Range With third paramater 
+#So, the third parameter in a Python for loop with range() is the step (how much the counter changes each iteration).
+for i in range(0, 10, 2):  
+    print(i)
