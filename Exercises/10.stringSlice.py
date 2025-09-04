08435fe5d5ef160dc861be569aef858aba7be86a
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-04 14:18:29 +0200

    strings slice

---
+++ Exercises/10.stringSlice.py
@@ -0,0 +1,17 @@
+fruit = "Mango"
+
+#for lenght of variable
+mangolen = len(fruit)
+
+print(mangolen)
+
+# print(fruit[0:4]) #including 0 but not 4
+# print(fruit[1:4]) #including 1 but not 4
+# print(fruit[:5])
+# print(fruit[0:-3])
+# print(fruit[:len(fruit)-3])
+
+#Quick Quiz
+
+nm = "harry"
+print(nm[-4:-2])
