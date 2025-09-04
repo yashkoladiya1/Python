68f2ed8033bd8cf130f7f2ce7301c36dddac2071
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-04 11:20:31 +0200

    strings

---
+++ Exercises/9.Strings.py
@@ -0,0 +1,33 @@
+# normal string
+a = "Yash"
+print("Normal string\n",a)
+
+# long string
+b = ''' hello i am boy
+and studying in the university
+
+i am yash
+ '''
+
+print("Long string\n",b)
+
+#accessing characters of a string
+c= "abcd"
+
+print(c[0])
+print(c[1])
+print(c[2])
+print(c[3])
+print("looping start")
+# print(c[4]) throws an error
+
+#looping through the string
+d = '''
+I am yash
+
+22 years old
+
+master student
+'''
+for character in d:
+    print(character)
