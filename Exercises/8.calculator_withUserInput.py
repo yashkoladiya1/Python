61995c6451dcec2ae7727bdcdc54e7854e0366b9
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-04 10:56:03 +0200

    user input calculator

---
+++ Exercises/8.calculator_withUserInput.py
@@ -0,0 +1,9 @@
+a = int(input("Enter A Value:"))
+b = int(input("Enter B Value:"))
+
+
+print("Addition of",a,"and",b,"\n=",a+b)
+print("Subtraction of",a,"and",b,"\n=",a-b)
+print("Division of",a,"and",b,"\n=",a/b)
+print("Multiplication of",a,"and",b,"\n=",a*b)
+print("Modulus of",a,"and",b,"\n=",a%b)
