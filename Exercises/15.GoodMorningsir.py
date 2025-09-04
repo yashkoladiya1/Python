d0c95516268b6c1f89e6019c8495557c4917b78a
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-04 17:47:50 +0200

    Good morning sir code

--- Exercises/15.GoodMorningsir.py
+++ Exercises/15.GoodMorningsir.py
@@ -2,13 +2,11 @@ import time
 
 HourStamp = int(time.strftime("%H"))
 
-if(HourStamp <= 12):
+if HourStamp < 12:
     print("Good Morning Sir")
-elif(HourStamp >=12):
-    if(HourStamp >= 17 and HourStamp<= 21):
-        print("Good evening Sir")
-    else:
-        print("Good Night Sir")
+elif 12 <= HourStamp < 17:
+    print("Good Afternoon Sir")
+elif 17 <= HourStamp <= 21:
+    print("Good Evening Sir")
 else:
-    print("Good after Noon sir")
-
+    print("Good Night Sir")
