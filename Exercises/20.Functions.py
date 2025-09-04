314ebf2531817a8672ad2d6033542cf21c476645
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-04 19:18:12 +0200

    functions

---
+++ Exercises/20.Functions.py
@@ -0,0 +1,15 @@
+def greaterthen(a,b):
+    if(a>b):
+        print("First number is greater")
+    else:
+        print("Second number is greate or eqaul too")
+
+
+a = 12
+b = 56
+greaterthen(a,b)
+
+
+# call function without body
+def func_name():
+    pass
