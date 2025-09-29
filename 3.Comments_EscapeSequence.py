2ad020a5e6e32b4bce113c248f61ff6e083cf84e
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-29 22:56:36 +0200

    Comments and Escape sequence

---
+++ 3.Comments_EscapeSequence.py
@@ -0,0 +1,18 @@
+# This is a "single line comment"
+print("Hey I am a good Boy and \n study masters") #printing the text
+
+
+
+#this is a "multiline comment"
+"""
+name yash
+occupation student
+surname Koladiya
+"""
+
+print("Hey My friend is \"bad boy\"")
+
+#More on PRINT Statement
+
+print("hey",6,7,sep="~",end="009\n")
+print("yash")
