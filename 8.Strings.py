9a99edf13aa5a4304c0fe50164736a91c4685da7
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-29 23:57:45 +0200

    Strings

---
+++ 8.Strings.py
@@ -0,0 +1,23 @@
+name = "yash"
+friend = "raj"
+
+print(name + friend)
+
+#multiline string
+a =  """
+Hello my name is yash
+I am 22 years old
+studing masters
+"""
+
+print("Printing multiline strings---->",a)
+
+#Indexing
+print("Indexing--->",name[0])
+print("Indexing--->",name[1])
+print("Indexing--->",name[2])
+print("Indexing--->",name[3])
+
+#Looping with strings
+for charachter in name:
+    print(charachter)
