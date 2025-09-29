76e6b65f70cc43553e2c2ee5dbb2767de4087205
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-29 22:26:13 +0200

    modules and pip theory

---
+++ 1.Module_pip.py
@@ -0,0 +1,14 @@
+#Pip is package manager
+
+#installation of modules syntax
+# pip install <Module_Name>
+
+"""
+After Installation 
+
+import modules in files
+"""
+
+# two types of modules
+# 1. built in module 
+# 2. external module (install from outside)
