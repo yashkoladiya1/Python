f37a7d7f74a0acd16c7932ac7e592835d1e56784
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-29 23:14:25 +0200

    Variable datatypes

---
+++ 4.Variables_DataTypes.py
@@ -0,0 +1,50 @@
+a = 1 #int
+b = True #boolean
+c = "Yash" #string 
+d = None #None
+
+print("Type of a is ",type(a))
+print("Type of b is ",type(b))
+print("Type of c is ",type(c))
+print("Type of d is ",type(d))
+
+# Data types
+"""
+1. Number : int,float,complex
+2. String
+3. Boolean
+4. Sequenced data: list,tuple
+5. Mapped data: dict
+"""
+
+# 1. Number datatypes
+e = 1
+f = 34.2
+g = 23j
+
+print("Type of e is ",type(e))
+print("Type of f is ",type(f))
+print("Type of g is ",type(g))
+
+#2 string
+name = "Yash"
+
+print("Type of Name is ",type(name))
+
+#3 boolean
+is_student = True
+
+print("Type of is_student is ",type(is_student))
+
+#4.1 list
+list1 = ["apple","banana","chiku"]
+
+print("Type of list1 is ",type(list1))
+
+#4.2 tuple
+tuple1 = ("apple","banana","chiku")
+print("Type of tuple1 is ",type(tuple1))
+
+#5 dict
+dict1 = {"name":"yash","age":22}
+print("Type of dict1 is ",type(dict1))
