8c11f57605ec09eb0602da92158d19edc4338e51
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-29 23:24:24 +0200

    simple calculator

---
+++ 5.Exercise_1.py
@@ -0,0 +1,7 @@
+a = 15
+b = 7
+
+print("Addtiton of ",a,"+" ,b,"is" ,a+b)
+print("Addtiton of ",a,"-" ,b,"is" ,a-b)
+print("Addtiton of ",a,"*" ,b,"is" ,a*b)
+print("Addtiton of ",a,"/" ,b,"is" ,a/b)
