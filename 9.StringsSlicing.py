3dd76777805e8e188eb6e372d7051e14ab650a89
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-30 18:22:54 +0200

    Strings slicings

---
+++ 9.StringsSlicing.py
@@ -0,0 +1,9 @@
+Fruit = "Mango"
+#length of string
+print("length of Fruit--->",len(Fruit))
+
+#slicing of string
+print("positive slicing---->",Fruit[1:4]) # positive slicing
+print("negative slicing---->",Fruit[-3:]) # negative slicing
+print("slicing from start--->",Fruit[:5]) #Slicing from start
+print("slicing from end--->",Fruit[4:]) #Slicing from end
