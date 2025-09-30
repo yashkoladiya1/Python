834cfb03cae1ac596d6198fab7e9a5cb5b53be36
yashkoladiya1 <koladiyayash104@gmail.com>
2025-09-30 19:44:19 +0200

    for loop

---
+++ 14.ForLoops.py
@@ -0,0 +1,17 @@
+#simple for loop
+colors = ["Red", "Green", "Yellow"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+
+#Range
+
+for k in range(5):
+    print(k + 1)
+
+for k in range(1, 10):
+    print(k)
+
+for k in range(1, 10, 2):
+    print(k)
