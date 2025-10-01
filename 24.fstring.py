47f96a433797bd4a3261233a6fb94b93af07dd4f
yashkoladiya1 <koladiyayash104@gmail.com>
2025-10-01 21:27:32 +0200

    docstrings

--- 24.fstring.py
+++ 24.fstring.py
@@ -4,3 +4,5 @@ name = "yash"
 print(f"hello my name is {name} and I am from {country}")
 
 
+price = 49
+print(f"mango price is {price:.2f}")
---
+++ 25.Docstrings.py
@@ -0,0 +1,8 @@
+def sqaure(n):
+    """ Takes in a number n , returns the sqaure of n
+    """
+    print(n**2)
+
+sqaure(5)
+
+print(sqaure.__doc__)
