e25ea9da29203a3780a17ea84844855b06d6159c
yashkoladiya1 <koladiyayash104@gmail.com>
2025-10-03 17:00:14 +0200

    shorthand if else

---
+++ 33.Shorthand_ifelse.py
@@ -0,0 +1,14 @@
+#Syntax
+"""
+
+value_if_true if condition else value_if_false
+
+"""
+age = 18
+status = "Adult" if age >= 18 else "Minor"
+print(status)   # Adult
+
+#Nested Shorthand
+score = 85
+result = "Excellent" if score >= 90 else "Good" if score >= 75 else "Fail"
+print(result)  # Good
